871e925704e96f14c5e6fa05ede9ff9e775ff18e
Denis Chetvertkov <chetvertkov-denis96@mail.ru>
2023-05-23 22:10:20 +0300

    Changed worker

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OnTaskDoneListener doneListener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(doneListener, errorListener);
+        worker.start();
     }
 }
